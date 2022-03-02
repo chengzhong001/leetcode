818b10d8c102ca0b4686c666f8574e08a60770f7
azhong <zczwzw@163.com>
2022-03-02 23:26:52 +0800

    update

--- 0007-reverse/python/Solution.py
+++ 0007-reverse/python/Solution.py
@@ -18,11 +18,23 @@ class Solution:
                 return 0
         return res
 
+    def reverse1(self, x: int) -> int:
+        minval = (-2) ** 31
+        maxval = 2**31 - 1
+        temp = str(x)
+        if x < 0:
+            return (
+                int("-" + temp[:0:-1])
+                if minval <= int("-" + temp[:0:-1]) <= maxval
+                else 0
+            )
+        else:
+            return int(temp[::-1]) if minval <= int(temp[::-1]) <= maxval else 0
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.reverse(123))
-    print(s.reverse(-123))
-    print(s.reverse(120))
-    print(s.reverse(0))
+    print(s.reverse1(123))
+    print(s.reverse1(-123))
+    print(s.reverse1(120))
+    print(s.reverse1(0))
