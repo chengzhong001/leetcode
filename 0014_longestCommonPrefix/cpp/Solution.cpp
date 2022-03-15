4bf87bfd6b57bfbc03b54c0e092480410048a879
azhong <zczwzw@163.com>
2022-03-15 23:29:48 +0800

    longestCommonPrefix

---
+++ 0014_longestCommonPrefix/cpp/Solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); ++i) {
+            while (strs[i].find(prefix) != 0) {
+                prefix.pop_back();
+                if (prefix.empty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    Solution s;
+    vector<string> strs = {"flower","flow","flight"};
+    cout << s.longestCommonPrefix(strs) << endl;
+
+    strs = {"dog","racecar","car"};
+    cout << s.longestCommonPrefix(strs) << endl;
+
+    return 0;
+}
